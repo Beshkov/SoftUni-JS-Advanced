1fae68025d0cb574f56f801f8060174329502c25
Aleksandar Beshkov <alexander.beshkov@gmail.com>
2021-10-31 11:46:05 +0200

    07

--- js-DOM intro/exercises/06. Format the Text/solution.js
+++ js-DOM intro/exercises/06. Format the Text/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO
+  // const textArea = document.getElementsByTagName('textarea')[0]
+  // let textAreaValue = textArea.value.split('.');
+  // let output = document.getElementById('output')
+  // let outputValue = ''
+  // for (let i = 0; i < textAreaValue.length; i+=3){
+  //   outputValue += `<p>${textAreaValue[i]+textAreaValue[i+1]+textAreaValue[i+2]}</p>`
+  // }
+  // output.innerHTML = outputValue
+
+  const textArea = document.getElementById("input");
+
+  let text = textArea.value;
+  let resultDiv = document.getElementById('output');
+
+  let sentance = text.split('.')
+  .filter(x => x !== "") // x.lenth > 1 works the same 
+  .map(x => x + '.');
+  console.log(text.split('.'));
+  let maxParagraphs = Math.ceil(sentance.length/3);
+
+  for (let i = 0; i < maxParagraphs; i++){
+    resultDiv.innerHTML += `<p>${sentance.splice(0, 3).join('')}</p>`
+  }
+  
+  console.log(resultDiv.innerText)
+  // there is a bug it return a new line \n and append a dot to it.
 }
--- js-DOM intro/exercises/07. Hell's Kitchen/app.js
+++ js-DOM intro/exercises/07. Hell's Kitchen/app.js
@@ -1,8 +1,54 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    document.querySelector('#btnSend').addEventListener('click', onClick);
+    // display restauratn of all the added one with its BEST and AVERAGE salarey.
+    // best restorant is the one with the highes Average salary.
+    // if two have the same the best is the 1st one added.
+    // display the workers in the best restauratns with there salaries - sorted by SALARIES in Descending order.
 
-   function onClick () {
-      //   TODO:
-      
-   }
+
+
+    function onClick() {
+        let text = JSON.parse(document.querySelector('#inputs textarea').value);
+        
+        let restaurants = {};
+
+        for (let i=0; i < text.length; i++){
+            let [restaurantName, workersString] = text[i].split(' - ')
+            let inputWorkers = workersString.split(', ').map(workerAndSalaryString => {
+                let [workerName, workerSalary] = workerAndSalaryString.split(' ');
+                return {workerName, salary: Number(workerSalary)};
+            })
+            
+            if (!restaurants[restaurantName]){
+                restaurants[restaurantName] = {
+                    workers: [],
+                    restaurantName: restaurantName,
+                    getAverageSalary: function(){
+                        return this.workers.reduce((acc, el) => acc + el.salary, 0)/this.workers.length
+                    }
+                };
+            }
+
+            restaurants[restaurantName].workers = restaurants[restaurantName].workers.concat(inputWorkers)         
+        }
+        // sort retaurants by averagesalary in descending order
+        let sortedRestaurants = Object.values(restaurants).sort((a,b) => b.getAverageSalary()-a.getAverageSalary())
+        let bestRestaurant = sortedRestaurants[0];
+        let sortedWorkers = bestRestaurant.workers.sort((a,b) => b.salary - a.salary);
+        let averageSalary = bestRestaurant.getAverageSalary().toFixed(2);
+        
+        let bestsalary = sortedWorkers[0].salary.toFixed(2);
+        let topRestaurant = `Name: ${bestRestaurant.restaurantName} Average Salary: ${averageSalary} Best Salary: ${bestsalary}`;
+        let workersToString = sortedWorkers.map(x => `Name: ${x.workerName} With Salary: ${x.salary}`).join(' ')
+
+        // insert best restaurtant in the DOM:  <p> - element 
+        let bestRestaurantPElement = document.querySelector('#bestRestaurant p');
+        let workersPElement = document.querySelector('#workers p');
+
+        bestRestaurantPElement.textContent = topRestaurant;
+        workersPElement.textContent = workersToString;
+    }
+
+
+
 }
