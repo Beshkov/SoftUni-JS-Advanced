614f85645249856db6d1e161b049b2cc93cb9cfb
Aleksandar Beshkov <alexander.beshkov@gmail.com>
2021-11-30 01:56:30 +0200

    events and dom manipulation

--- js-DOM-Events/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ js-DOM-Events/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,27 @@
 function focused() {
-    console.log('TODO:...');
-}
+    // select all input div-s 
+    //change the class to focused 
+    // Array.from(document.getElementsByTagName('input'))
+    //     .forEach(el =>{
+    //         el.addEventListener('focus', ({target: { parentNode } }) => parentNode.classList.add('focused'));
+    //         el.addEventListener('blur', ({target: { parentNode } }) => parentNode.classList.remove('focused'));
+    //         el.addEventListener('focus', ({target: { parentNode } }) => console.log(parentNode));
+    //         console.log(el);
+            
+    //     })
+
+    const fields = Array.from(document.getElementsByTagName('input'));
+
+    for ( let field of fields ) {
+        field.addEventListener('focus', onFocus)
+        field.addEventListener('blur', onBlur)
+    }
+
+    function onBlur(ev) {
+        ev.target.parentNode.classList.remove('focused')
+    }
+
+    function onFocus(ev) {
+        ev.target.parentNode.classList.add('focused');
+    }
+};
--- js-DOM-Events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ js-DOM-Events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-   //TODO...
+   document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkOut)
+
+   const cart = [];
+   const output = document.getElementsByTagName('textarea')[0];
+   output.value = '';
+
+   function onClick(ev) {
+      if (ev.target.tagName == 'BUTTON' && ev.target.classList.contains('add-product')){
+         const product = ev.target.parentNode.parentNode;
+         const name = product.querySelector('.product-title').textContent;
+         const price = Number(product.querySelector('.product-line-price').textContent);
+
+         cart.push({
+            name,
+            price
+         });
+
+         output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`; 
+      }  
+   };
+
+   function checkOut() {
+      const product = new Set();
+      cart.forEach(p => product.add(p.name));
+
+      const totalPrice = cart.reduce((ttl, cur) => ttl + cur.price,0);
+      
+      output.value += `You bought ${[...product.keys()].join(', ')} for ${totalPrice.toFixed(2)}.`;
+   };
 }
